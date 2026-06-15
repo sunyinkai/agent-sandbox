c61efd99d5d996e6772f74b2e1467597fa100050
yinkaisun <yinkaisun@qq.com>
2026-06-15 23:19:11 +0800

    week02: add a langraph demo

---
+++ week02_langgraph_repair/graph_demo.py
@@ -0,0 +1,44 @@
+from langgraph.graph import StateGraph,START,END
+from operator import add
+from typing import TypedDict,Annotated
+
+class MyState(TypedDict):
+    id: int
+    file: str
+    step: int
+    messages: Annotated[list[str], add]
+
+def greet_node(state:MyState)-> dict:
+    step = state.get("step", 0)
+    print(f"greetings! your file is {state['file']}")
+    return {
+        "step": step+1,
+        "messages": ["greeting finished\n"]
+    }
+
+def compile_node(state:MyState) -> dict:
+    print(f"begin to compile {state['file']}")
+    return {
+        "step": state['step']+1,
+        "messages": ["compile finished\n"]
+    }
+def fix_node(state:MyState) -> dict:
+    print(f"begin to fix {state['file']}")
+    return {
+        "step": state['step']+1,
+        "messages": ["fix finished\n"]
+    }
+
+builder = StateGraph(MyState)
+builder.add_node("greet",greet_node)
+builder.add_node("compile",compile_node)
+builder.add_node("fix",fix_node)
+
+builder.add_edge(START,"greet")
+builder.add_edge("greet","compile")
+builder.add_edge("compile","fix")
+builder.add_edge("fix",END)
+
+graph = builder.compile()
+result = graph.invoke({'id':0,'file':"hello.py","step":0, "messages":[]})
+print(f"result={result}")
